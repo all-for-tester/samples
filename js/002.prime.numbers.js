e785f93970f491a61571746a53580916be215520
all-for-tester <all.for.tester@gmail.com>
2020-12-03 22:14:03 +0300

    fixed prime numbers w/function

--- js/002.prime.numbers.js
+++ js/002.prime.numbers.js
@@ -6,31 +6,17 @@ for (var i = 1; i <= max; i++) {
     if (a == true) {
         console.log(i);
     }
-
-    let b = checkIfNumberIsNotPrime(i);
-    if (b == false) {
-        console.log(i);
-    }
 }
 
-let newNumber = 11002;
-let cc = checkIfNumberIsPrime(newNumber);
+// let newNumber = 11002;
+// let cc = checkIfNumberIsPrime(newNumber);
 
 function checkIfNumberIsPrime(number) {
-    let flagIfPrime = true;
+    let flagIfPrime = true;    
     for (let j = 2; j < number; j++) {
         if (number % j == 0) {
-            flagIfPrime = false;
+            flagIfPrime = false;            
         }
     }
     return flagIfPrime;
-}
-
-function checkIfNumberIsNotPrime(number) {
-    for (let j = 2; j < number; j++) {
-        if (number % j == 0) {
-            return true;
-        }
-    }
-    return false;
 }
